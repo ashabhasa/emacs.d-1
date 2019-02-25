43239440c63a7c1a4324e7adf99ac20f87957009
Arber Shabhasa <ashabhasa@gmail.com>
2019-02-25 16:56:09 +0100

    Add configuration for scala. and xml formatting

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -85,6 +85,51 @@
 ;; Use easy-mark in place of mark-sexp
 (global-set-key [remap mark-sexp] 'easy-mark)
 
+;; Diable lock files
+(setq create-lockfiles nil)
+
+
+;; scala configuration
+(require-package 'scala-mode)
+(add-to-list 'auto-mode-alist
+             '("\.scala" . scala-mode)
+             '("\.sbt\'" . scala-mode))
+
+(add-hook 'scala-mode-hook
+          (lambda ()
+            ;; (show-paren-mode)
+            ;; (smartparens-mode)
+            ;; (yas-minor-mode)
+            ;; (git-gutter-mode)
+            ;; (company-mode)
+            (scala-mode:goto-start-of-code)))
+
+;; Add some whitespace padding depending on what you
+;; type after `(` or `{` parenthesis
+(require-package 'smartparens)
+(require 'smartparens-config)
+(sp-local-pair 'scala-mode "(" nil :post-handlers '(("||\n[i]" "RET")))
+(sp-local-pair 'scala-mode "{" nil :post-handlers '(("||\n[i]" "RET") ("| " "SPC")))
+
+;; restrict the smartparens navigation commands
+;; to just the brackets (ignoring Emacs’ s-expression
+;; interpretation of the Scala language as provided by scala-mode)
+(defun sp-restrict-c (sym)
+  "Smartparens restriction on `SYM' for C-derived parenthesis."
+  (sp-restrict-to-pairs-interactive "{([" sym))
+;; end of scala configuration
+
+
+;; xml configuration
+(require 'init-nxml)
+(require 'nxml-mode)
+(define-key nxml-mode-map (kbd "C-c m f") 'sanityinc/pp-xml-region)
+(define-key nxml-mode-map (kbd "C-c m b") 'sanityinc/tidy-buffer-xml)
+
+;; end of xml configuration
+
+
+
 ;;; Themes
 ;;(require-package 'doom-themes)
 ;;(load-theme 'doom-tomorrow-night t)
