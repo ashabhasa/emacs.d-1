a4b307ad2a3bacff9af06c9f2391bea274305468
Arber Shabhasa <ashabhasa@gmail.com>
2019-10-04 11:45:14 +0200

    Enable xml outline manipulation in nXml mode.

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -126,6 +126,25 @@
 (define-key nxml-mode-map (kbd "C-c m f") 'sanityinc/pp-xml-region)
 (define-key nxml-mode-map (kbd "C-c m b") 'sanityinc/tidy-buffer-xml)
 
+;; Enable hiding and showing of xml nodes using C-c h short cut
+(require 'hideshow)
+(require 'sgml-mode)
+(require 'nxml-mode)
+
+(add-to-list 'hs-special-modes-alist
+             '(nxml-mode
+               "<!--\\|<[^/>]*[^/]>"
+               "-->\\|</[^/>]*[^/]>"
+
+               "<!--"
+               sgml-skip-tag-forward
+               nil))
+
+(add-hook 'nxml-mode-hook 'hs-minor-mode)
+
+;; optional key bindings, easier than hs defaults
+(define-key nxml-mode-map (kbd "C-c h") 'hs-toggle-hiding)
+
 ;; end of xml configuration
 
 
